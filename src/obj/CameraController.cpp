c96de2206b4d412f38ed43f2dff9d84b341c7b04
Chase W <chasew@silkweaver.com>
2011-09-16 12:16:15 -0400

    Made the camera not leave the map.

--- src/obj/CameraController.cpp
+++ src/obj/CameraController.cpp
@@ -1,5 +1,6 @@
 #include "obj/CameraController.h"
 
+#include "scenes/SceneGame.h"
 #include "obj/Player.h"
 
 namespace obj
@@ -33,8 +34,33 @@ namespace obj
 			case Npc::Right: pos.x += renderOffset;  break;
 			default: break;
 		}
+		
+		sf::Vector2i layerSize( game.layers[ 0 ].GetTiles().size(), game.layers[ 0 ].GetTiles()[ 0 ].size() );
+		sf::Vector2i visibleTiles( Game::WindowSize.x / Game::TileSize, Game::WindowSize.y / Game::TileSize );
+		sf::Vector2i halfScreen( ( visibleTiles.x / 2 ) * Game::TileSize, ( visibleTiles.y / 2 ) * Game::TileSize );
+		
+		if ( pos.x < halfScreen.x )
+		{
+			pos.x = halfScreen.x;
+		}
+		else if ( pos.x > ( layerSize.x * Game::TileSize ) - halfScreen.x )
+		{
+			pos.x = ( layerSize.x * Game::TileSize ) - halfScreen.x;
+		}
+		
+		if ( pos.y < halfScreen.y )
+		{
+			pos.y = halfScreen.y;
+		}
+		else if ( pos.y > ( layerSize.y * Game::TileSize ) - halfScreen.y - Game::TileSize )
+		{
+			pos.y = ( layerSize.y * Game::TileSize ) - halfScreen.y - Game::TileSize;
+		}
+		std::cout << pos.y << std::endl;
+		
 		pos.x = static_cast< int >( pos.x );
-		pos.y = static_cast< int >( pos.y );
+		pos.y = static_cast< int >( pos.y + ( Game::TileSize / 2 ) );
+		
 		view.SetCenter( pos );
 		window.SetView( view );
 	}
