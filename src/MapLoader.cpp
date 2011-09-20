ecb89c2082a1166d4ebd6013a7a9006076303bcc
Kake_Fisk <orjan_lo@hotmail.com>
2011-09-20 23:55:37 +0200

    Added .c_str()

--- src/MapLoader.cpp
+++ src/MapLoader.cpp
@@ -15,7 +15,7 @@ MapLoader::MapLoader( Game& theGame, SceneGame& theScene, std::vector< TileLayer
      objects( theObjects )
 {
 }
-		
+
 bool MapLoader::LoadMap( const std::string& mapName )
 {
 	xml::Document doc;
@@ -23,15 +23,15 @@ bool MapLoader::LoadMap( const std::string& mapName )
 	{
 		return false;
 	}
-	
+
 	xml::Node& root = doc.GetRootNode();
-	
+
 	std::string widthStr = xml::GetAttribute( root, "width" ).GetValue();
 	std::string heightStr = xml::GetAttribute( root, "height" ).GetValue();
-	
+
 	width = util::FromString< size_t >( widthStr );
 	height = util::FromString< size_t >( heightStr );
-	
+
 	auto children = root.GetChildren();
 	for ( auto it = children.begin(); it != children.end(); ++it )
 	{
@@ -51,32 +51,32 @@ bool MapLoader::LoadMap( const std::string& mapName )
 			}
 		}
 	}
-	
+
 	return true;
 }
 
 bool MapLoader::LoadXml( xml::Document& doc, const std::string& filename )
 {
-	std::fstream file( filename, std::fstream::in );
+	std::fstream file( filename.c_str(), std::fstream::in );
 	if ( !file )
 	{
 		return false;
 	}
-	
+
 	std::string contents;
 	while ( true )
 	{
 		std::string line;
 		std::getline( file, line );
-		
+
 		if ( file.eof() )
 		{
 			break;
 		}
-		
+
 		contents += line;
 	}
-	
+
 	return std::get< 0 >( doc.Parse( contents ) );
 }
 
@@ -89,9 +89,9 @@ bool MapLoader::ParseTileLayer( xml::Node& node )
 	{
 		return false;
 	}
-	
+
 	std::vector< std::string > tileData;
-	
+
 	auto children = node.GetChildren();
 	for ( auto it = children.begin(); it != children.end(); ++it )
 	{
@@ -100,33 +100,33 @@ bool MapLoader::ParseTileLayer( xml::Node& node )
 		{
 			continue;
 		}
-		
+
 		std::vector< std::string > tokens = util::Tokenize( node.GetText(), " " );
 		tileData.insert( tileData.end(), tokens.begin(), tokens.end() );
 	}
-	
+
 	if ( tileData.size() < width * height )
 	{
 		return false;
 	}
-	
+
 	TileLayer layer( sf::Vector2i( width, height ), tileTypes[ 0 ] );
 	for ( auto it = tileData.begin(); it != tileData.end(); ++it )
 	{
 		size_t dist = std::distance( tileData.begin(), it );
 		size_t row = dist / width;
 		size_t col = dist % width;
-		
+
 		if ( row >= height )
 		{
 			break;
 		}
-		
+
 		sf::Uint8 index = util::FromString< sf::Uint16 >( * it ); // 16 instead of 8 because 8 is really a char...
 		layer[ col ][ row ] = tileTypes[ index ];
 	}
 	layers.push_back( layer );
-	
+
 	return true;
 }
 
@@ -137,7 +137,7 @@ std::vector< Tile > MapLoader::LoadTileTypes( sf::Texture& tex, const std::strin
 	{
 		return std::vector< Tile >();
 	}
-	
+
 	auto children = doc.GetRootNode().GetChildren();
 	std::vector< Tile > tiles( 256 );
 	for ( auto it = children.begin(); it != children.end(); ++it )
@@ -147,16 +147,16 @@ std::vector< Tile > MapLoader::LoadTileTypes( sf::Texture& tex, const std::strin
 		{
 			continue;
 		}
-		
+
 		std::string indexStr = xml::GetAttribute( node, "index" ).GetValue();
 		std::string collideStr = xml::GetAttribute( node, "collide" ).GetValue();
-		
+
 		sf::Uint8 index = util::FromString< sf::Uint16 >( indexStr ); // 16 instead of 8 because 8 is really a char...
 		bool collide = util::FromString< bool >( collideStr );
-		
+
 		tiles[ index ] = Tile( tex, index, collide );
 	}
-	
+
 	return tiles;
 }
 
@@ -174,7 +174,7 @@ bool MapLoader::ParseStaticObjects( xml::Node& node )
 			}
 		}
 	}
-	
+
 	return true;
 }
 
@@ -183,14 +183,14 @@ bool MapLoader::ParseSceneryObject( xml::Node& node )
 	sf::Texture& tex = game.GetTexture( "scenery/" + xml::GetAttribute( node, "file" ).GetValue() + ".png" );
 	std::string xStr = xml::GetAttribute( node, "x" ).GetValue();
 	std::string yStr = xml::GetAttribute( node, "y" ).GetValue();
-	
+
 	float x = util::FromString< float >( xStr );
 	float y = util::FromString< float >( yStr );
-	
+
 	sf::Vector2f pos( x * Game::TileSize, y * Game::TileSize );
-	
+
 	boost::shared_ptr< obj::Base > scenery( new obj::RenderObject( scene, tex, pos ) );
 	objects.push_back( scenery );
-	
+
 	return true;
 }
