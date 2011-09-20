199416cd9d8ef153fbb5c21626263487cc9fc2f7
Chase W <chasew@silkweaver.com>
2011-09-20 14:30:32 -0400

    Map loader interface.

---
+++ src/MapLoader.h
@@ -0,0 +1,20 @@
+#ifndef MAPLOADER_H
+#define MAPLOADER_H
+
+#include "TileLayer.h"
+#include "SceneGame.h"
+
+class MapLoader
+{
+	public:
+		MapLoader( std::vector< TileLayer >& layers, SceneGame::ObjectList& objects );
+		
+		bool LoadFromFile( const std::string& mapName );
+	
+	private:
+		std::vector< TileLayer >& layers;
+		SceneGame::ObjectList& objects;
+		
+};
+
+#endif // MAPLOADER_H
