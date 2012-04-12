a780d0bb66649db1f3bc7866fd227219a47e0a52
Chase W <chasew@silkweaver.com>
2012-04-11 20:05:49 -0400

    Render player.

--- src/game/renderers/PlayerRenderer.cpp
+++ src/game/renderers/PlayerRenderer.cpp
@@ -1,11 +1,21 @@
 #include "game/renderers/PlayerRenderer.h"
 
+#include <SFML/Graphics.hpp>
+
+#include "game/objects/Player.h"
+#include "ResourceManager.h"
+
 PlayerRenderer::PlayerRenderer( std::shared_ptr< GameObject > theObject )
-   : BaseRenderer::BaseRenderer( theObject )
+   : BaseRenderer::BaseRenderer( theObject ),
+     player( std::dynamic_pointer_cast< Player >( theObject ) )
 {
+	spr.setTexture( * ResourceManager::getTexture( "res/character/male.png" ) );
+	spr.setTextureRect( sf::IntRect( 0, 0, 32, 64 ) );
+	spr.setOrigin( 16, 48 );
 }
 
 void PlayerRenderer::render( sf::RenderWindow& window, const World& world )
 {
-	//
+	spr.setPosition( player->getPosition().x * 32, player->getPosition().y * 32 );
+	window.draw( spr );
 }
--- src/game/renderers/PlayerRenderer.h
+++ src/game/renderers/PlayerRenderer.h
@@ -1,7 +1,11 @@
 #ifndef PLAYERRENDERER_H
 #define PLAYERRENDERER_H
 
+#include <SFML/Graphics/Sprite.hpp>
+
 #include "game/renderers/BaseRenderer.h"
+
+class Player;
 
 class PlayerRenderer : public BaseRenderer
 {
@@ -9,6 +13,10 @@ class PlayerRenderer : public BaseRenderer
 		PlayerRenderer( std::shared_ptr< GameObject > theObject );
 		
 		virtual void render( sf::RenderWindow& window, const World& world );
+	
+	private:
+		std::shared_ptr< Player > player;
+		sf::Sprite spr;
 };
 
 #endif // PLAYERRENDERER_H
